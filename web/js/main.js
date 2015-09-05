aba4e7aa2edb0e6871e16d652ede8d5ea837c23a
Roosevelt <rdt-2012@hotmail.com>
2015-09-05 11:42:09 -0500

    - funcoin de multiplicar

--- web/js/main.js
+++ web/js/main.js
@@ -1,24 +1,33 @@
-$(document).ready(function(){
-  // $('#mvcIcon').hide();
-  $('#mvcIcon .mvcPointer').click(function(){
-    $('#mvcMain').toggle(150);
-    $('#mvcIcon').toggle(150);
-  });
-  $('#mvcMain .mvcPointer').click(function(){
-    $('#mvcMain').toggle(150);
-    $('#mvcIcon').toggle(150);
-  });
+$(document).ready(function () {
+    // $('#mvcIcon').hide();
+    $('#mvcIcon .mvcPointer').click(function () {
+        $('#mvcMain').toggle(150);
+        $('#mvcIcon').toggle(150);
+    });
+    $('#mvcMain .mvcPointer').click(function () {
+        $('#mvcMain').toggle(150);
+        $('#mvcIcon').toggle(150);
+    });
 });
 
 //funcion para cerrar la ventana de error automaticamente
 function closeError() {
     $('div[class="alert alert-danger alert-dismissible"]').alert('close');
 }
-function closeSucess(){
+function closeSucess() {
     $('div[class="alert alert-success alert-dismissible"]').alert('close');
 }
 $(document).ready(function () {
     setTimeout(closeError, 8000);
     setTimeout(closeSucess, 8000);
 });
+
+function multiplicarCampos() {
+
+    var text1 = document.getElementById("text1").value;
+    var text2 = document.getElementById("text2").value;
+    var text3 = text1 * text2;
+    document.getElementById("text3").value = text3;
+
+}
 // FIN DE LA FUNCION
