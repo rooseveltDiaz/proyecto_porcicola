19032ffc53c9ddef35fcd45fe1d95da35e087e41
Roosevelt <rdt-2012@hotmail.com>
2015-09-17 23:52:33 -0500

    - Correcion funcion de aumento de peso

--- controller/animal/createRegistroPesoActionClass.php
+++ controller/animal/createRegistroPesoActionClass.php
@@ -17,83 +17,73 @@ use hook\log\logHookClass as log;
  */
 class createRegistroPesoActionClass extends controllerClass implements controllerActionInterface {
 
-  public function execute() {
-    try {
-      if (request::getInstance()->isMethod('POST')) {
-        // DATOS DE ANIMAL
-        $fecha = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::FECHA, true));
-        $empleado = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::EMPLEADO, true));
-        $animal = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::ANIMAL, true));
-        $valor_kilo = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::KILO, true));
+    public function execute() {
+        try {
+            if (request::getInstance()->isMethod('POST')) {
+                // DATOS DE ANIMAL
+                $fecha = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::FECHA, true));
+                $empleado = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::EMPLEADO, true));
+                $animal = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::ANIMAL, true));
+                $valor_kilo = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::KILO, true));
 //        $valor_total = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::VALOR, true));
-        $peso = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::PESO, true));
-       $valor_total = $peso * $valor_kilo;
-         
-       registroPesoTableClass::validateCreate($fecha, $empleado,  $peso, $valor_kilo);
-       
-       $datos = array(
-          $fecha,
-          $empleado,
-          $animal,
-          $valor_kilo,
-         $valor_total,
-          $peso
-          
-        );
-      
+                $peso = request::getInstance()->getPost(registroPesoTableClass::getNameField(registroPesoTableClass::PESO, true));
+                $valor_total = $peso * $valor_kilo;
 
-   
-        $data = array(
-            registroPesoTableClass::FECHA => $fecha,
-            registroPesoTableClass::EMPLEADO => $empleado,
-            registroPesoTableClass::ANIMAL => $animal,
-            registroPesoTableClass::KILO => $valor_kilo,
-           registroPesoTableClass::VALOR => $valor_total,
-            registroPesoTableClass::PESO => $peso
-        );
-        registroPesoTableClass::insert($data);
-        
-        $fields = array(
-        hojaVidaTableClass::ANIMAL,
-        hojaVidaTableClass::FECHA_NACIMIENTO,
-        hojaVidaTableClass::GENERO_ID,
-        hojaVidaTableClass::NUMERO,
-        hojaVidaTableClass::PARTO,
-        hojaVidaTableClass::PESO,
-        hojaVidaTableClass::RAZA
-        );
-        $where = array(
-        hojaVidaTableClass::ID => $animal
-        );
-        
-        $daticos = hojaVidaTableClass::getAll($fields, FALSE, NULL, NULL, NULL, NULL, $where);
-        // se pasa los datos del ID animal para Actualizar la hoja de vida
-        $data = array(
-//        hojaVidaTableClass::FECHA_NACIMIENTO => $daticos[hojaVidaTableClass::getNameField(hojaVidaTableClass::FECHA_NACIMIENTO,true)],
-//        hojaVidaTableClass::GENERO_ID => $daticos[hojaVidaTableClass::getNameField(hojaVidaTableClass::GENERO_ID,true)],
-//        hojaVidaTableClass::NUMERO => $daticos[hojaVidaTableClass::getNameField(hojaVidaTableClass::NUMERO, true)],
-//        hojaVidaTableClass::PARTO => $daticos[hojaVidaTableClass::getNameField(hojaVidaTableClass::PARTO,true)],
-//        hojaVidaTableClass::RAZA => $daticos[hojaVidaTableClass::getNameField(hojaVidaTableClass::RAZA, true)],
-        hojaVidaTableClass::PESO=> $peso
-        );
-        $ids = array(
-        hojaVidaTableClass::ID =>$animal
-        );
-        hojaVidaTableClass::update($ids, $data);
-        //FIN 
-        
-        session::getInstance()->setSuccess(i18n::__('succesCreate1', null, 'dpVenta'));
-        log::register(i18n::__('create'), registroPesoTableClass::getNameTable());
-        routing::getInstance()->redirect('animal', 'indexRegistroPeso', array(hojaVidaTableClass::getNameField(hojaVidaTableClass::ANIMAL) => $animal));
-      } else {
-        log::register(i18n::__('create'), registroPesoTableClass::getNameTable(), i18n::__('errorCreateBitacora'));
-        session::getInstance()->setError(i18n::__('errorCreate1', null, 'dpVenta'));
-        routing::getInstance()->redirect('animal', 'indexRegistroPeso');
-      }
-    } catch (PDOException $exc) {
-      session::getInstance()->setFlash('exc', $exc);
-      routing::getInstance()->forward('shfSecurity', 'exception');
+                registroPesoTableClass::validateCreate($fecha, $empleado, $peso, $valor_kilo);
+
+//                $datos = array(
+//                    $fecha,
+//                    $empleado,
+//                    $animal,
+//                    $valor_kilo,
+//                    $valor_total,
+//                    $peso
+//                );
+
+                $dataPeso = array(
+                    registroPesoTableClass::FECHA => $fecha,
+                    registroPesoTableClass::EMPLEADO => $empleado,
+                    registroPesoTableClass::ANIMAL => $animal,
+                    registroPesoTableClass::KILO => $valor_kilo,
+                    registroPesoTableClass::VALOR => $valor_total,
+                    registroPesoTableClass::PESO => $peso
+                );
+                registroPesoTableClass::insert($dataPeso);
+
+                $fieldsAnimal = array(
+                    hojaVidaTableClass::NUMERO,
+                    hojaVidaTableClass::PESO,
+                );
+                $whereAnimal = array(
+                    hojaVidaTableClass::ANIMAL => $animal
+                );
+
+                $objAnimalHojaDeVida = hojaVidaTableClass::getAll($fieldsAnimal, FALSE, NULL, NULL, NULL, NULL, $whereAnimal);
+
+                $peso = $peso + $objAnimalHojaDeVida[0]->peso_animal;
+//                exit();
+                // se pasa los datos del ID animal para Actualizar la hoja de vida
+                $data = array(
+                    hojaVidaTableClass::PESO => $peso
+                );
+                $ids = array(
+                    hojaVidaTableClass::ANIMAL => $animal
+                );
+                hojaVidaTableClass::update($ids, $data);
+                //FIN 
+
+                session::getInstance()->setSuccess(i18n::__('succesCreate1', null, 'dpVenta'));
+                log::register(i18n::__('create'), registroPesoTableClass::getNameTable());
+                routing::getInstance()->redirect('animal', 'indexRegistroPeso', array(hojaVidaTableClass::getNameField(hojaVidaTableClass::ANIMAL) => $animal));
+            } else {
+                log::register(i18n::__('create'), registroPesoTableClass::getNameTable(), i18n::__('errorCreateBitacora'));
+                session::getInstance()->setError(i18n::__('errorCreate1', null, 'dpVenta'));
+                routing::getInstance()->redirect('animal', 'indexRegistroPeso');
+            }
+        } catch (PDOException $exc) {
+            session::getInstance()->setFlash('exc', $exc);
+            routing::getInstance()->forward('shfSecurity', 'exception');
+        }
     }
-  }
 
 }
